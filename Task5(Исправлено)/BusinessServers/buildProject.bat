c54b39248bf320f06365c19e9735d52f4328fb74
PNVB <nvbperm@yandex.ru>
2019-04-26 10:43:32 +0500

    Исправлено задание №5

---
+++ Task5(Исправлено)/BusinessServers/buildProject.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+pushd "C:\Users\vpolischuk\Desktop\’…‘’ˆƒ\Code\ThermoObject\ThermoObject\BusinessServers"
+msbuild "C:\Users\vpolischuk\Desktop\’…‘’ˆƒ\Code\ThermoObject\ThermoObject\BusinessServers\ThermoObject.BusinessServers.csproj"
---
+++ Task5(Исправлено)/Objects/buildProject.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+pushd "C:\Users\vpolischuk\Desktop\’…‘’ˆƒ\Code\ThermoObject\ThermoObject\Objects"
+msbuild "C:\Users\vpolischuk\Desktop\’…‘’ˆƒ\Code\ThermoObject\ThermoObject\Objects\ThermoObject.Objects.csproj"
